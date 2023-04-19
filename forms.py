bf0a69762eb43a6ba7e6ef74f8209fcb560a9dff
waltpianist97 <walterbrnrd@gmail.com>
2023-04-19 22:32:32 +0200

    adjust a delete method and form

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField,TextAreaField,DateField
-from wtforms.validators import InputRequired, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField,TextAreaField,FloatField
+from wtforms.validators import DataRequired, Email, EqualTo
 from models import User
 
 CHOICES = [('5', """Il giro che stai registrando è una gara ufficiale o un evento ufficiale di Team (con locandina)?"""),
@@ -24,9 +24,9 @@ class LoginForm(FlaskForm):
 
 class NewTripForm(FlaskForm):
     tripname = StringField('Trip name',validators=[DataRequired()])
-    speed = StringField('Average speed [km/h]',validators=[DataRequired()])
-    distance = StringField('Distance [km]',validators=[DataRequired()])
-    elevation = StringField('Elevation [m]',validators=[DataRequired()])
+    speed = FloatField('Average speed [km/h]',validators=[DataRequired()])
+    distance = FloatField('Distance [km]',validators=[DataRequired()])
+    elevation = FloatField('Elevation [m]',validators=[DataRequired()])
     prestige = RadioField('Choose an option', choices=CHOICES, validators=[DataRequired()])
     description = TextAreaField('Description')
     submit = SubmitField('Add trip')
