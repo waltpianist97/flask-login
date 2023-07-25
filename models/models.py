545a2f0d006cee25423ef64a086af824c8963341
waltpianist97 <walterbrnrd@gmail.com>
2023-07-25 11:25:10 +0200

    add strava_id in the trips, so that it fetches only the latest trips from strava

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Integer,Column,String,DateTime,Float,Boolean,ForeignKey, BLOB
+from sqlalchemy import Integer, Column, String, DateTime, Float, Boolean, ForeignKey, BLOB
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -8,13 +8,14 @@ import os
 import shutil
 from tools import send_email_utility, AUTO_MAIL
 
+
 class TeamUserAssociation(db.Model):
     __tablename__ = 'team_user_association'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
     team_id = Column(Integer, ForeignKey('team.id', ondelete='CASCADE'))
-    role =Column(String(140))
+    role = Column(String(140))
     join_date = Column(DateTime)
     current_ranking = Column(Integer)
 
@@ -24,7 +25,8 @@ class AdminMixin:
     def is_admin(self):
         return self._is_admin == True
 
-class User(db.Model,UserMixin,AdminMixin):
+
+class User(db.Model, UserMixin, AdminMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(140))
     name = Column(String(140))
@@ -34,39 +36,45 @@ class User(db.Model,UserMixin,AdminMixin):
     password = Column(String(140))
     password_hash = Column(String(140))
     phone_number = Column(String(140))
-    trips = relationship('Trip',backref='user',lazy='dynamic',cascade="all, delete-orphan")
-    teams = relationship('Team', secondary="team_user_association", back_populates='users')
-    join_requests = relationship("RequestsToJoinTeam", back_populates="user", cascade="all, delete-orphan")
+    trips = relationship('Trip', backref='user',
+                         lazy='dynamic', cascade="all, delete-orphan")
+    teams = relationship(
+        'Team', secondary="team_user_association", back_populates='users')
+    join_requests = relationship(
+        "RequestsToJoinTeam", back_populates="user", cascade="all, delete-orphan")
     profile_picture = Column(String(140))
     profile_background = Column(String(140))
     profile_banner = Column(String(140))
 
-    _is_admin = Column(Boolean,nullable=True)
+    _is_admin = Column(Boolean, nullable=True)
 
-    def set_password(self,password):
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self,password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
-        
+
     def get_role_in_team(self, team_id):
-        user_associated = TeamUserAssociation.query.filter_by(team_id=team_id, user_id=self.id).first()
+        user_associated = TeamUserAssociation.query.filter_by(
+            team_id=team_id, user_id=self.id).first()
         if user_associated:
             return user_associated.role
         else:
             return None
-    
-    def set_role_in_team(self, team_id,role):
-        user_associated = TeamUserAssociation.query.filter_by(team_id=team_id, user_id=self.id).first()
+
+    def set_role_in_team(self, team_id, role):
+        user_associated = TeamUserAssociation.query.filter_by(
+            team_id=team_id, user_id=self.id).first()
         if user_associated:
-            user_associated.role=role
-            
+            user_associated.role = role
+
     def group_user_trips_by_team(self):
-        result=[] 
+        result = []
         for team in self.teams:
-            trips_in_team = [trip for trip in self.trips if trip.team_id==team.id] 
-            result.append({"team_name": team.name,"team_id":team.id, "trips_by_team": trips_in_team})
+            trips_in_team = [
+                trip for trip in self.trips if trip.team_id == team.id]
+            result.append(
+                {"team_name": team.name, "team_id": team.id, "trips_by_team": trips_in_team})
         return result
 
     def create_pictures_folder(self):
@@ -74,7 +82,7 @@ class User(db.Model,UserMixin,AdminMixin):
         parent_folder = os.path.dirname(os.path.dirname(current_file_path))
         pics_folder_path = f"{parent_folder}/images/users/{self.username}"
         os.mkdir(pics_folder_path)
-    
+
     def delete_pictures_folder(self):
         current_file_path = os.path.realpath(__file__)
         parent_folder = os.path.dirname(os.path.dirname(current_file_path))
@@ -84,67 +92,77 @@ class User(db.Model,UserMixin,AdminMixin):
 
     def get_trips(self):
         return self.trips
-    
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-  
+
 
 class Trip(db.Model):
     id = Column(Integer, primary_key=True)
+    strava_id = Column(Integer)
     tripname = Column(String(140))
-    speed = Column(Float(precision=2),nullable=False)
-    distance = Column(Float(precision=2),nullable=False)
-    elevation = Column(Float(precision=2),nullable=False)
-    prestige = Column(Integer,nullable=False)
+    speed = Column(Float(precision=2), nullable=False)
+    distance = Column(Float(precision=2), nullable=False)
+    elevation = Column(Float(precision=2), nullable=False)
+    prestige = Column(Integer, nullable=False)
     description = Column(String(140))
     recorded_on = Column(DateTime, index=True, default=datetime.utcnow)
     n_of_partecipants = Column(Integer, nullable=False, default=1)
     user_id = Column(Integer, ForeignKey('user.id', ondelete="CASCADE"))
     team_id = Column(Integer, ForeignKey('team.id', ondelete="CASCADE"))
-    score = Column(Integer,default=0)
-    is_approved = Column(Boolean,default=False)
-    placements = relationship("PlacementsInTrip", backref="trip", cascade="all, delete-orphan")
+    score = Column(Integer, default=0)
+    is_approved = Column(Boolean, default=False)
+    placements = relationship(
+        "PlacementsInTrip", backref="trip", cascade="all, delete-orphan")
     n_of_placements = Column(Integer)
-    
+
     def __repr__(self):
         return '<Trip {}>'.format(self.description)
 
     def get_user(self):
         return User.query.get(self.user_id)
-    
+
     def get_team(self):
         return Team.query.get(self.team_id)
 
     @staticmethod
-    def calculate_score(v,dx,dz,pr,np,piazzamenti):
+    def calculate_score(v, dx, dz, pr, np, piazzamenti):
         """dx: distance in km,
         dz: the height in m,
         pr: the prestige, 
         np: the number of players, 
         v: the velocity in km/h
         piazzamenti: the list of top placements"""
-        l=5e-3                              
-        d=2.5e-4
-        punteggio_finale_o = lambda v,dx,dz,pr: dx*l*(dz*d+0.25)*(0.7+0.15*(pr-3)) if v<=25.0 else dx*l*(dz*d+0.25)*(0.7+0.15*(pr-3))*(1+0.05*(v-25.0))
-        punteggio_finale_f = lambda punteggio_finale_o,np,piazzamenti: int(round((punteggio_finale_o + (np-1)/10)*100,0))+11*len(piazzamenti)-sum(piazzamenti)
+        l = 5e-3
+        d = 2.5e-4
 
-        p_f_o = punteggio_finale_o(v,dx,dz,pr)
-        return punteggio_finale_f(p_f_o,np,piazzamenti)
+        def punteggio_finale_o(v, dx, dz, pr): return dx*l*(dz*d+0.25)*(0.7+0.15*(
+            pr-3)) if v <= 25.0 else dx*l*(dz*d+0.25)*(0.7+0.15*(pr-3))*(1+0.05*(v-25.0))
+        def punteggio_finale_f(punteggio_finale_o, np, piazzamenti): return int(round(
+            (punteggio_finale_o + (np-1)/10)*100, 0))+11*len(piazzamenti)-sum(piazzamenti)
+
+        p_f_o = punteggio_finale_o(v, dx, dz, pr)
+        return punteggio_finale_f(p_f_o, np, piazzamenti)
 
     def get_placements(self):
         return PlacementsInTrip.query.filter_by(trip_id=self.id).all()
+
 
 class PlacementsInTrip(db.Model):
     id = Column(Integer, primary_key=True)
     trip_id = Column(Integer, ForeignKey('trip.id', ondelete="CASCADE"))
     place = Column(Integer)
 
+
 class Team(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(140))
-    trips = relationship('Trip',backref='team',lazy='dynamic',cascade="all, delete-orphan")
-    users = relationship('User', secondary="team_user_association", back_populates='teams')
-    join_requests = relationship("RequestsToJoinTeam", back_populates="team", cascade="all, delete-orphan")
+    trips = relationship('Trip', backref='team',
+                         lazy='dynamic', cascade="all, delete-orphan")
+    users = relationship(
+        'User', secondary="team_user_association", back_populates='teams')
+    join_requests = relationship(
+        "RequestsToJoinTeam", back_populates="team", cascade="all, delete-orphan")
     description = Column(String(140))
     team_picture = Column(String(140))
     team_background = Column(String(140))
@@ -154,51 +172,60 @@ class Team(db.Model):
     def __repr__(self):
         return '<Team {}>'.format(self.description)
 
-    def add_member(self, member:User,role:str,join_date:datetime = None):
-        t_u_association = TeamUserAssociation(team_id=self.id,user_id=member.id,role=role,join_date=join_date,current_ranking=len(self.users)+1)
+    def add_member(self, member: User, role: str, join_date: datetime = None):
+        t_u_association = TeamUserAssociation(
+            team_id=self.id, user_id=member.id, role=role, join_date=join_date, current_ranking=len(self.users)+1)
         db.session.add(t_u_association)
         db.session.commit()
 
-    def set_member_ranking(self,member:User,current_ranking:int):
-        t_u_association = TeamUserAssociation.query.filter_by(user_id=member.id,team_id=self.id).first()
+    def set_member_ranking(self, member: User, current_ranking: int):
+        t_u_association = TeamUserAssociation.query.filter_by(
+            user_id=member.id, team_id=self.id).first()
         previous_ranking = t_u_association.current_ranking
         if previous_ranking != current_ranking:
-            send_email_utility('Cambio ranking',f"Il tuo ranking è cambiato nel team {self.name}! Sei passato dal {previous_ranking}° al {current_ranking}° posto!",AUTO_MAIL,member.email)
+            send_email_utility(
+                'Cambio ranking', f"Il tuo ranking è cambiato nel team {self.name}! Sei passato dal {previous_ranking}° al {current_ranking}° posto!", AUTO_MAIL, member.email)
 
         t_u_association.current_ranking = current_ranking
         db.session.commit()
 
     def ranking_builder(self):
-        members_by_team = User.query.filter(User.id.in_([member.id for member in self.users])).all()
+        members_by_team = User.query.filter(User.id.in_(
+            [member.id for member in self.users])).all()
         ranking_list = []
         for user_by_team in members_by_team:
-            all_scores_by_user = Trip.query.filter_by(user_id=user_by_team.id,team_id=self.id,is_approved=True).all()
-            tot_score_by_user =sum([score_by_user.score for score_by_user in all_scores_by_user])
-            ranking_list.append({"user_id":user_by_team.id,"user":user_by_team.username,"total score":tot_score_by_user,"user_obj":user_by_team})
-        ranking_list = list(enumerate(sorted(ranking_list, key=lambda x: x['total score'],reverse=True)))
+            all_scores_by_user = Trip.query.filter_by(
+                user_id=user_by_team.id, team_id=self.id, is_approved=True).all()
+            tot_score_by_user = sum(
+                [score_by_user.score for score_by_user in all_scores_by_user])
+            ranking_list.append({"user_id": user_by_team.id, "user": user_by_team.username,
+                                "total score": tot_score_by_user, "user_obj": user_by_team})
+        ranking_list = list(
+            enumerate(sorted(ranking_list, key=lambda x: x['total score'], reverse=True)))
 
-        for place,ranking  in ranking_list:
-            self.set_member_ranking(ranking["user_obj"],place+1)
-
+        for place, ranking in ranking_list:
+            self.set_member_ranking(ranking["user_obj"], place+1)
 
         return ranking_list
 
     def get_leaders(self):
-        leaders = User.query.join(TeamUserAssociation).filter(TeamUserAssociation.team_id == self.id, TeamUserAssociation.role == "team_leader").all()
+        leaders = User.query.join(TeamUserAssociation).filter(
+            TeamUserAssociation.team_id == self.id, TeamUserAssociation.role == "team_leader").all()
         return leaders
-    
+
     def create_pictures_folder(self):
         current_file_path = os.path.realpath(__file__)
         parent_folder = os.path.dirname(os.path.dirname(current_file_path))
         pics_folder_path = f"{parent_folder}/images/teams/{self.name}"
         os.mkdir(pics_folder_path)
-        
+
     def delete_pictures_folder(self):
         current_file_path = os.path.realpath(__file__)
         parent_folder = os.path.dirname(os.path.dirname(current_file_path))
         pics_folder_path = f"{parent_folder}/images/teams/{self.name}"
         if os.path.exists(pics_folder_path):
             shutil.rmtree(pics_folder_path)
+
 
 class RequestsToJoinTeam(db.Model):
     __tablename__ = 'requests_to_join_team'
@@ -211,5 +238,3 @@ class RequestsToJoinTeam(db.Model):
 
     user = relationship("User", back_populates="join_requests")
     team = relationship("Team", back_populates="join_requests")
-
-
--- routes/routes_user_area.py
+++ routes/routes_user_area.py
@@ -1,77 +1,78 @@
 from app import app, db, strava_client
-from flask import request, render_template, flash, redirect,url_for,session
-from flask_login import current_user,login_required
+from flask import request, render_template, flash, redirect, url_for, session
+from flask_login import current_user, login_required
 from sqlalchemy import or_, and_, desc, func
 from models import User, Trip, Team, TeamUserAssociation, RequestsToJoinTeam
 from forms import *
 from werkzeug.urls import url_parse
 from datetime import datetime
-from tools import send_email_utility,AUTO_MAIL, PictureUploader, get_or_create_strava_client
+from tools import send_email_utility, AUTO_MAIL, PictureUploader, get_or_create_strava_client
 import os
 from pathlib import Path
 import os
 import warnings
 
 
-@app.route('/user_home/<username>',methods=['GET', 'POST'])
+@app.route('/user_home/<username>', methods=['GET', 'POST'])
 @login_required
 def user_home(username):
-    
+
     user = User.query.filter_by(username=current_user.username).first()
-    #trips = Trip.query.filter_by(user_id=current_user.id).all()
+    # trips = Trip.query.filter_by(user_id=current_user.id).all()
     teams = Team.query.all()
     message = ""
     """if user_teams:
         if user in team.users and RequestsToJoinTeam.query.filter_by(team_id = team.id).all():
             message = "You have some pending enrollment requests, check out the team page!"
     """
-    last_trips = Trip.query.order_by(desc(Trip.recorded_on)).filter_by(user_id = user.id).limit(3).all()
-    
+    last_trips = Trip.query.order_by(desc(Trip.recorded_on)).filter_by(
+        user_id=user.id).limit(3).all()
+
     if last_trips is None:
         last_trips = []
     if teams is None:
         teams = []
-    stat={}
-    stat["average_speed"] = db.session.query(Trip, func.avg(Trip.speed)).group_by(Trip.user_id).filter_by(user_id=user.id).all()
+    stat = {}
+    stat["average_speed"] = db.session.query(Trip, func.avg(Trip.speed)).group_by(
+        Trip.user_id).filter_by(user_id=user.id).all()
     if stat["average_speed"]:
-        stat["average_speed"] = round(stat["average_speed"][0][1],2)
-    stat["maximum_elevation"] = db.session.query(Trip, func.max(Trip.elevation)).group_by(Trip.user_id).filter_by(user_id=user.id).all()
+        stat["average_speed"] = round(stat["average_speed"][0][1], 2)
+    stat["maximum_elevation"] = db.session.query(Trip, func.max(
+        Trip.elevation)).group_by(Trip.user_id).filter_by(user_id=user.id).all()
     if stat["maximum_elevation"]:
-        stat["maximum_elevation"]= stat["maximum_elevation"][0][1]
-    stat["total_distance"] = db.session.query(Trip, func.sum(Trip.distance)).group_by(Trip.user_id).filter_by(user_id=user.id).all()
+        stat["maximum_elevation"] = stat["maximum_elevation"][0][1]
+    stat["total_distance"] = db.session.query(Trip, func.sum(
+        Trip.distance)).group_by(Trip.user_id).filter_by(user_id=user.id).all()
     if stat["total_distance"]:
-        stat["total_distance"]= round(stat["total_distance"][0][1],2)
+        stat["total_distance"] = round(stat["total_distance"][0][1], 2)
     stat["activities"] = len(Trip.query.filter_by(user_id=user.id).all())
 
-    return render_template('user_home.html', user=user,teams=teams,new_enrollments=message, last_trips=last_trips,stat=stat)
+    return render_template('user_home.html', user=user, teams=teams, new_enrollments=message, last_trips=last_trips, stat=stat)
+
 
 @app.route('/delete_file/<target>/<file_db_column>/<path:filepath>', methods=['GET', 'POST'])
 @app.route('/delete_file/<target>/<file_db_column>/<path:filepath>/<int:team_id>', methods=['GET', 'POST'])
 @login_required
-def delete_file(target,file_db_column,filepath,team_id=None):
+def delete_file(target, file_db_column, filepath, team_id=None):
 
     picture_uploader = None
 
     if not team_id:
-        picture_uploader = PictureUploader(target,current_user.username)
+        picture_uploader = PictureUploader(target, current_user.username)
         picture_uploader.delete_file(filepath)
         if hasattr(current_user, file_db_column):
             setattr(current_user, file_db_column, None)
-            db.session.commit() 
+            db.session.commit()
         return redirect(url_for('user_profile'))
 
     else:
         team = Team.query.get(team_id)
-        picture_uploader = PictureUploader(target,team.name)
+        picture_uploader = PictureUploader(target, team.name)
         picture_uploader.delete_file(filepath)
         if hasattr(team, file_db_column):
             setattr(team, file_db_column, None)
-            db.session.commit() 
-        return redirect(url_for('team_profile',team_id=team_id))
-
-
-
-
+            db.session.commit()
+        return redirect(url_for('team_profile', team_id=team_id))
 
 
 @app.route('/user_profile', methods=['GET', 'POST'])
@@ -80,7 +81,7 @@ def user_profile():
     user = User.query.get(current_user.id)
     form = ProfileForm(obj=user)
     teams = Team.query.all()
-    picture_uploader = PictureUploader("users",current_user.username)
+    picture_uploader = PictureUploader("users", current_user.username)
     global strava_client
     is_strava_client_in_synch = False
     if strava_client:
@@ -94,7 +95,7 @@ def user_profile():
         user.email = form.email.data
         user.strava_account = form.strava_account.data
         user.phone_number = form.phone_number.data
-     
+
         if form.profile_picture.data:
             file = form.profile_picture.data
             picture_uploader.delete_file(user.profile_picture)
@@ -113,80 +114,82 @@ def user_profile():
             filename = picture_uploader.load_file(file)
             user.profile_banner = f'users/{current_user.username}/{filename}'
 
-    
-
         db.session.commit()
         flash("Il tuo profilo e' stato aggiornato!", 'success')
         return redirect(url_for('user_profile'))
-    return render_template('user_profile.html', form=form,teams=teams,is_strava_in_synch=is_strava_client_in_synch)
+    return render_template('user_profile.html', form=form, teams=teams, is_strava_in_synch=is_strava_client_in_synch)
 
 
-@app.route('/trips_overview/<int:user_id>',methods=['GET', 'POST'])
+@app.route('/trips_overview/<int:user_id>', methods=['GET', 'POST'])
 @login_required
 def trips_overview(user_id):
-    
+
     user = User.query.filter_by(id=user_id).first()
-    result=user.group_user_trips_by_team()
+    result = user.group_user_trips_by_team()
     teams = Team.query.all()
 
-    return render_template('trips_overview.html', user=user, trips_groups=result,teams=teams)
- 
+    return render_template('trips_overview.html', user=user, trips_groups=result, teams=teams)
 
 
-@app.route('/unenroll_from_team/<int:team_id>/<int:user_id>',methods=['GET', 'POST'])
+@app.route('/unenroll_from_team/<int:team_id>/<int:user_id>', methods=['GET', 'POST'])
 @login_required
-def unenroll_from_team(team_id,user_id):
-    
-    team =  Team.query.get(team_id)
+def unenroll_from_team(team_id, user_id):
+
+    team = Team.query.get(team_id)
     user = User.query.get(user_id)
-    trips = Trip.query.filter_by(team_id=team_id,user_id=user_id).all()
-    
+    trips = Trip.query.filter_by(team_id=team_id, user_id=user_id).all()
+
     if current_user in team.users or user in team.users:
         [db.session.delete(trip) for trip in trips]
         team.users.remove(user)
         db.session.commit()
-        
-        send_email_utility("Deregistrazione dal team",f"Sei stato deregistrato dal team {team.name}!",AUTO_MAIL,user.email)
+
+        send_email_utility("Deregistrazione dal team",
+                           f"Sei stato deregistrato dal team {team.name}!", AUTO_MAIL, user.email)
     if current_user._is_admin:
-        return redirect(url_for("view_user_profile_by_admin",user_id=user.id))
+        return redirect(url_for("view_user_profile_by_admin", user_id=user.id))
     else:
         if current_user == user:
-            return redirect(url_for("team_home",team_id=team_id))
-        else:   
-            return redirect(url_for("manage_team",team_id=team_id))
+            return redirect(url_for("team_home", team_id=team_id))
+        else:
+            return redirect(url_for("manage_team", team_id=team_id))
 
 
-@app.route('/request_enrollment_to_team/<int:team_id>',methods=['GET', 'POST'])
+@app.route('/request_enrollment_to_team/<int:team_id>', methods=['GET', 'POST'])
 @login_required
 def request_enrollment_to_team(team_id):
-    
-    team = Team.query.get(team_id)
-    user_req= RequestsToJoinTeam.query.filter_by(team_id = team_id, user_id = current_user.id).first()
-    if user_req:
-        return redirect(url_for("team_home",team_id=team_id))
 
-    req = RequestsToJoinTeam(team_id = team_id,user_id = current_user.id,status="pending",request_date=datetime.now())
+    team = Team.query.get(team_id)
+    user_req = RequestsToJoinTeam.query.filter_by(
+        team_id=team_id, user_id=current_user.id).first()
+    if user_req:
+        return redirect(url_for("team_home", team_id=team_id))
+
+    req = RequestsToJoinTeam(team_id=team_id, user_id=current_user.id,
+                             status="pending", request_date=datetime.now())
 
     if current_user not in team.users:
         db.session.add(req)
         db.session.commit()
 
         emails_leaders = [tl.email for tl in team.get_leaders()]
-        send_email_utility('Richiesta di registrazione nel team', f"{current_user.username} ha richiesto di registrarsi nel {team.name}, controlla la tua pagina 'gestisci squadra'!",AUTO_MAIL,emails_leaders)
-        return redirect(url_for("team_home",team_id=team_id,requests_to_join=req))
+        send_email_utility('Richiesta di registrazione nel team',
+                           f"{current_user.username} ha richiesto di registrarsi nel {team.name}, controlla la tua pagina 'gestisci squadra'!", AUTO_MAIL, emails_leaders)
+        return redirect(url_for("team_home", team_id=team_id, requests_to_join=req))
 
-@app.route('/withdraw_request_enrollment/<int:request_id>/<int:team_id>',methods=['GET', 'POST'])
+
+@app.route('/withdraw_request_enrollment/<int:request_id>/<int:team_id>', methods=['GET', 'POST'])
 @login_required
-def withdraw_request_enrollment(request_id,team_id):
-    
+def withdraw_request_enrollment(request_id, team_id):
+
     request_to_remove = RequestsToJoinTeam.query.get(request_id)
     db.session.delete(request_to_remove)
     db.session.commit()
 
-    return redirect(url_for("team_home",team_id=team_id))
+    return redirect(url_for("team_home", team_id=team_id))
 
 
-@app.route('/strava_synch',methods=["GET","POST"])
+@app.route('/strava_synch', methods=["GET", "POST"])
 @login_required
 def strava_synch():
     global strava_client
@@ -197,14 +200,18 @@ def strava_synch():
         flash("Profilo non sincronizzato con Strava")
     return redirect(url_for("user_profile"))
 
-@app.route('/trips_from_strava',methods=["GET","POST"])
+
+@app.route('/trips_from_strava', methods=["GET", "POST"])
 @login_required
 def trips_from_strava():
     global strava_client
     if strava_client:
-        last_trip_date = datetime(2023, 3, 1, 0, 0, 0)
-        activities = strava_client.get_logged_in_athlete_activities(after=last_trip_date)
-        return render_template('trips_from_strava.html', activities=activities)
+        activities = strava_client.get_logged_in_athlete_activities()
+        existing_strava_ids = [
+            trip.strava_id for trip in current_user.get_trips()]
+        activities_not_in_db = [
+            activity for activity in activities if activity.id not in existing_strava_ids]
+        return render_template('trips_from_strava.html', activities=activities_not_in_db)
     else:
         flash("Sincronizza il tuo account con strava prima di sincronizzare i giri!")
         return redirect(url_for("user_profile"))
