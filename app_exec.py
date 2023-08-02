f7f45110f0ed6179a8e8992110b1abb488369633
waltpianist97 <walterbrnrd@gmail.com>
2023-08-02 17:05:14 +0200

    eliminate useless imports in app_exec

--- app_exec.py
+++ app_exec.py
@@ -1,13 +1,13 @@
 from app import app, login_manager
-from routes import routes_general_scope,routes_team_area,routes_user_area,routes_admin_area
+from routes import routes_general_scope, routes_team_area, routes_user_area, routes_admin_area
 from models import User
-from flask import g
+
 
 @login_manager.user_loader
 def load_user(id):
-    
-    return User.query.get(int(id))
-  
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    return User.query.get(int(id))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
